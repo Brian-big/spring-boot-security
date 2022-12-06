7a9d7596f87a6e2a4bb56bba5ce79c3c14d23de2
Brian Barasa <briannbig@gmail.com>
2022-12-06 22:49:29 +0300

    Initial Commit

---
+++ src/main/java/com/workshop/brianSec/BrianSecApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.brianSec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrianSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrianSecApplication.class, args);
+	}
+
+}
